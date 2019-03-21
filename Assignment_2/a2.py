49eca2d0518db2a94b858339f25be2a486023ab5
waynehost <waynezjiang@gmail.com>
2019-03-21 23:26:04 +1100

    comments added

--- Assignment_2/a2.py
+++ Assignment_2/a2.py
@@ -1,5 +1,5 @@
 '''
-COMP9321 2019 Term 1 Assignment Two Code Template
+COMP9321 2019 Term 1 Assignment Two
 Student Name: Zhou JIANG
 Student ID: z5146092
 '''
@@ -13,9 +13,10 @@ import re
 import urllib.request as req
 
 app = Flask(__name__)
-api = Api(app, title="World Bank", description="API for World Bank Economic Indicators.")
+api = Api(app, title="World Bank", description="API for World Bank Economic Indicators. Zhou JIANG z5146092")
 
 
+# database controller, for executing SQL command and fetching results
 def database_controller(database, command):
     connection = sqlite3.connect(database)
     cursor = connection.cursor()
@@ -29,6 +30,7 @@ def database_controller(database, command):
     return result
 
 
+# database initialization
 def create_db(db_file):
     if os.path.exists(db_file):
         print('Database already exists.')
@@ -53,6 +55,7 @@ def create_db(db_file):
     return True
 
 
+# remote API request, fetching data from worldbank API
 def remote_request(indicator, page, start=2013, end=2018, content_format='json'):
     url = f'http://api.worldbank.org/v2/countries/all/indicators/' + \
           f'{indicator}?date={start}:{end}&format={content_format}&page={page}'
@@ -63,6 +66,7 @@ def remote_request(indicator, page, start=2013, end=2018, content_format='json')
     return json.loads(data)[1]
 
 
+# a template function, convert SQL query result to json-like format, format 1
 def collection_table_json_template(query_result):
     return {"location": "/{}/{}".format(query_result[1], query_result[0]),
             "collection_id": "{}".format(query_result[0]),
@@ -71,6 +75,7 @@ def collection_table_json_template(query_result):
             }
 
 
+# a template function, convert SQL query result to json-like format, format 2
 def retrieve_one_json_template(collection_query, entries_query):
     result = {"collection_id": "{}".format(collection_query[0]),
               "indicator": "{}".format(collection_query[2]),
@@ -86,6 +91,7 @@ def retrieve_one_json_template(collection_query, entries_query):
     return result
 
 
+# uniform request handler, all requests received will be firstly handled by this function
 def request_handler(database, collection, action, **kwargs):
     if action == 'post':
         return post_handler(database, collection, kwargs['indicator'])
@@ -108,10 +114,13 @@ def request_handler(database, collection, action, **kwargs):
             return get_handler(database, collection, 'gettopbottom', collection_id=kwargs['collection_id'],
                                year=kwargs['year'], flag='bottom', value=bottom_test.group(2))
         else:
-            return {"message": "Your input arguments are not in correct format!"}, 400
+            return {"message": "Your input arguments are not in correct format! Must be either top<int> or bottom<int>."}, 400
 
 
+# dealing with all get requests, for question 3-6
 def get_handler(database, collection, action, **kwargs):
+    
+    # question 3, get all collections info
     if action == 'getall':
         query = database_controller(database, f"SELECT * FROM Collection WHERE collection_name ='{collection}';")
         if query:
@@ -121,6 +130,7 @@ def get_handler(database, collection, action, **kwargs):
             return result_list, 200
         return {"message": f"The collection '{collection}' not found in data source!"}, 404
 
+    # question 4, get one specified collection and its data
     elif action == 'getone':
         collection_query = database_controller(database,
                                                f"SELECT * "
@@ -137,6 +147,7 @@ def get_handler(database, collection, action, **kwargs):
         return {"message":
                 f"The collection '{collection} with id {kwargs['collection_id']}'not found in data source!"}, 404
 
+    # question 5, get data for specified year, id and country
     elif action == 'getoneyc':
         join_query = database_controller(database,
                                          f"SELECT collection_id, indicator, country, date, value "
@@ -155,6 +166,7 @@ def get_handler(database, collection, action, **kwargs):
         return {"message":
                 f"The given arguments '{kwargs}' not found in data source!"}, 404
 
+    # question 6, get data for specified year, id, sort by its value, can be either descent or ascent.
     elif action == 'gettopbottom':
         insert_flag = ''
         if kwargs['flag'] == 'bottom':
@@ -183,6 +195,7 @@ def get_handler(database, collection, action, **kwargs):
                 f"No data matches your specified arguments in the database!"}, 404
 
 
+# dealing with all post requests, for question 1
 def post_handler(database, collection, indicator):
     query = database_controller(database, f"SELECT * FROM Collection WHERE indicator = '{indicator}';")
     if query:
@@ -204,6 +217,7 @@ def post_handler(database, collection, indicator):
         return collection_table_json_template(new_query[0]), 201
 
 
+# dealing with all delete requests, for question 2
 def delete_handler(database, collection, collection_id):
     query = database_controller(database,
                                 f"SELECT * FROM Collection WHERE collection_name = '{collection}' "
@@ -216,6 +230,7 @@ def delete_handler(database, collection, collection_id):
         return {"message": f"Collection = {collection_id} is removed from the database!"}, 200
 
 
+# importing data, store in database for table collection, called by post handler
 def collection_table_updater(database, given_id, given_collection_name, data):
     cur_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     collection = "INSERT INTO Collection VALUES ({}, '{}', '{}', '{}', '{}');"\
@@ -224,6 +239,7 @@ def collection_table_updater(database, given_id, given_collection_name, data):
     database_controller(database, collection)
 
 
+# importing data, store in database for table collection, called by post handler
 def entry_table_updater(database, given_id, data):
     entry = "INSERT INTO Entries VALUES"
     for sub_data in data:
@@ -237,6 +253,7 @@ parser = api.parser()
 parser.add_argument('q', type=str, help='Your query here (e.g."top10")', location='args')
 
 
+# single-path route class, for question 1 and 3
 @api.route("/<collections>")
 class SingleRoute(Resource):
     @api.expect(post_model)
@@ -249,6 +266,7 @@ class SingleRoute(Resource):
         return request_handler('data.db', collections, 'getall')
 
 
+# double-paths route class, for question 2 and 4
 @api.route("/<collections>/<collection_id>")
 class DualRoute(Resource):
     def delete(self, collections, collection_id):
@@ -258,6 +276,7 @@ class DualRoute(Resource):
         return request_handler('data.db', collections, 'getone', collection_id=collection_id)
 
 
+# quad-paths route class, for question 5
 @api.route("/<collections>/<collection_id>/<year>/<country>")
 class QuadRoute(Resource):
     def get(self, collections, collection_id, year, country):
@@ -265,6 +284,7 @@ class QuadRoute(Resource):
                                year=year, country=country)
 
 
+# triple-paths route class, with argument required, for question 6
 @api.route("/<collections>/<collection_id>/<year>")
 @api.doc(parser=parser)
 class ArgsRoute(Resource):
